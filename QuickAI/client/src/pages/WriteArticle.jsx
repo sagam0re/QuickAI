91817e4231d92572feb6185bb5047276677d9417
Giorgi <giorgi.janelidze97@gmail.com>
2025-09-25 18:07:21 +0400

    feat: added server implementation to frontend, sent the genrate article api request

--- QuickAI/client/src/pages/WriteArticle.jsx
+++ QuickAI/client/src/pages/WriteArticle.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
 import { Sparkles, Edit } from 'lucide-react'
 import { useState } from 'react'
+import axios from 'axios'
+import { useAuth } from '@clerk/clerk-react'
+import toast from 'react-hot-toast'
+import Markdown from 'react-markdown';
+
+axios.defaults.baseURL = import.meta.env.VITE_SERVER_DEV_URL;
+
 
 function WriteArticle() {
 
@@ -14,8 +21,35 @@ function WriteArticle() {
     const [selectedLength, setSelectedLength] = useState(articleLength[0]);
     const [input, setInput] = useState('');
 
+    const [loading, setLoading] = useState(false);
+    const [content, setContent] = useState('');
+
+    const {getToken} = useAuth();
+
     const onSubmitHandler = async (e) => {
         e.preventDefault();
+        try {
+            setLoading(true);
+            const prompt = `Write an article about ${input} with length of ${selectedLength.length} words.`;
+            const token = await getToken();
+            const {data} = await axios.post('/api/ai/generate-article', {
+                prompt,
+                length: selectedLength.length
+            }, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            });
+            if(data.success) {
+                setContent(data.content);
+            } else {
+                toast.error(data.message || 'Something went wrong!');
+            }
+        } catch (error) {
+            toast.error(error.message || 'Something went wrong!');
+        } finally {
+            setLoading(false);
+        }
     }
 
     return (
@@ -46,9 +80,11 @@ function WriteArticle() {
                     ))}
                 </div>
                 <br />
-                <button className='w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#226BFF] to-[#65ADFF] 
+                <button disabled={loading} className='w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#226BFF] to-[#65ADFF] 
                                     text-white px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer'>
-                    <Edit className='w-5' />
+                                        {loading ? <span className='w-4 h-4 my-1 rounded-full border-2 border-t-transparent animate-spin'>
+
+                                        </span> : <Edit className='w-5' />}
                     Generate Article
                 </button>
             </form>
@@ -58,12 +94,18 @@ function WriteArticle() {
                         <Edit className='w-5 h-5 text-[#4A7AFF]' />
                         <h1 className='text-xl font-semibold'>Generated Article</h1>
                     </div>
-                    <div className='flex-1 flex justify-center items-center'>
+                    {!content ? (<div className='flex-1 flex justify-center items-center'>
                         <div className='text-sm flex flex-col items-center gap-5 text-gray-400'>
                         <Edit className='w-9 h-9' />
                         <p>No article generated yet.</p>
                         </div>
-                    </div>
+                    </div>) : 
+                    (<div className='mt-3 h-full overflow-y-scroll text-sm text-slate-600'>
+                        <div className='.reset-tw'>
+                            <Markdown>{content}</Markdown>
+                        </div>
+                    </div>)}
+                    
             </div>
         </div>
     )
